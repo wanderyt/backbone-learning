26e2c066bd1d3682fdc3ac84b8a6c917dfe2b7fb
David Ren <wanderyt1988@163.com>
2016-07-01 15:36:15 +0800

    Add model research continued.

---
+++ backboneModelsSample2/js/model.js
@@ -0,0 +1,30 @@
+﻿var Book = Backbone.Model.extend({
+    defaults: {
+        ID: "",
+        BookName: ""
+    },
+
+    idAttribute: "ID",
+
+    initialize: function () {
+        console.log('Book has been intialized');
+
+        this.on("invalid", function (model, error) {
+            console.log("Houston, we have a problem: " + error)
+        });
+    },
+
+    constructor: function (attributes, options) {
+        console.log('Book\'s constructor had been called');
+        Backbone.Model.apply(this, arguments);
+    },
+
+    validate: function (attr) {
+        if (attr.ID <= 0) {
+            return "Invalid value for ID supplied."
+        }
+    },
+
+    urlRoot: 'http://localhost:51377/api/Books'
+});
+
