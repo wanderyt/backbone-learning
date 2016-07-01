d78886fe094156be4cb431b7d0302e8da30d0ad0
David Ren <wanderyt1988@163.com>
2016-07-01 15:35:46 +0800

    Add collection research.

---
+++ backboneCollectionsSample/js/model.js
@@ -0,0 +1,40 @@
+﻿var Book = Backbone.Model.extend({
+    defaults: {
+        ID: "",
+        BookName: ""
+    },
+    idAttribute: "ID",
+   
+    urlRoot: 'http://localhost:51377/api/Books'
+});
+
+
+var BooksCollection = Backbone.Collection.extend({
+    model: Book,
+
+    initialize: function () {
+
+        // This will be called when an item is added. pushed or unshifted
+        this.on('add', function (model) {
+            console.log('something got added');
+        });
+
+        // This will be called when an item is removed, popped or shifted
+        this.on('remove', function (model) {
+            console.log('something got removed');
+        });
+
+        // This will be called when an item is updated
+        this.on('change', function (model) {
+            console.log('something got changed');
+        });
+    },
+
+    comparator: function (model) {
+        return model.get("ID");
+    },
+
+    url: "http://localhost:51377/api/Books",
+});
+
+
