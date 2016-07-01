321d588c2adc800689c2310d8e83dec4627d46c2
David Ren <wanderyt1988@163.com>
2016-07-01 13:48:18 +0800

    Test model initialization

---
+++ backboneModels/js/model.js
@@ -0,0 +1,30 @@
+﻿var Book = Backbone.Model.extend({
+    defaults: {
+        ID: "",
+        BookName: ""
+    },
+
+    initialize: function () {
+        console.log('Book has been intialized');
+
+        // Lets hook up some event handers to listen to model change
+        this.on('change:BookName', function () {
+            console.log('Message from specific listener: BookName has been changed');
+        });
+
+        // Lets hook up some event handers to listen to model change
+        this.on('change', function () {
+            if (this.hasChanged('ID')) {
+                console.log('ID has been changed');
+            }
+            if (this.hasChanged('BookName')) {
+                console.log('BookName has been changed');
+            }
+        });
+    },
+
+    showAlert: function () {
+        alert('ID: ' + this.get('ID') + ', BookName: ' + this.get('BookName'));
+    }
+});
+
